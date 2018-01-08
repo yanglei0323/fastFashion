aa252614e0c1a3b8ea1b5fa387e0ecc2a0a6ec3c
yanglei0323 <1085943717@qq.com>
2018-01-08 15:07:57 +0800

    基础框架搭建

---
+++ app.js
@@ -0,0 +1,15 @@
+App({
+  onLaunch: function () {
+    // console.log('App Launch')// 小程序启动之后 触发
+  },
+  onShow: function () {
+    // console.log('App Show')
+  },
+  onHide: function () {
+    // console.log('App Hide')
+  },
+  globalData: {
+    
+  }
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,80 @@
+// pages/index/index.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '/image/b1.jpg',
+      '/image/b2.jpg',
+      '/image/b3.jpg'
+    ],
+    indicatorDots: true, //是否显示面板指示点
+    autoplay: true, //是否自动切换
+    interval: 3000, //自动切换时间间隔,3s
+    interval1: 2000,
+    duration: 1000, //  滑动动画时长1s
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '11111',
+      desc: '222222',
+      path: '',
+      imageUrl: '',
+
+    }
+  }
+})
